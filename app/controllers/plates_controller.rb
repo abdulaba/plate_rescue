7cf63e5ff10be1cf4e0cca4a47a95d417a85b126
(Sofia Alfaro) sofiavapalfaro98@gmail.com <sofiavapalfaro98@gmail.com>
2024-01-10 10:21:35 -0500

    avanzando con el crud de plates

--- app/controllers/plates_controller.rb
+++ app/controllers/plates_controller.rb
@@ -1,2 +1,37 @@
 class PlatesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @plate = Plate.new
+  end
+
+  def create
+  @restaurant = Restaurant.find(params[:restaurant_id])
+    @plate = Plate.new(plate_params)
+    @plate.restaurant_id = @restaurant.id
+    if @plate.save
+      redirect_to restaurant_path(@restaurant.id)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+  
+  private
+  def dish_params
+    params.require(:dish).permit(:name, :description, :price, :photo)
+  end
 end
