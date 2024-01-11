286a48e371a23ae59b702817220226f5b18bfd09
(Sofia Alfaro) sofiavapalfaro98@gmail.com <sofiavapalfaro98@gmail.com>
2024-01-11 13:49:13 -0500

    cart

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,49 +1,33 @@
 class RestaurantsController < ApplicationController
-
   def index
     @restaurants = Restaurant.all
-
-    @markers = @restaurants.geocoded.map do |restaurant|
-      {
-        lat: restaurant.latitude,
-        lng: restaurant.longitude,
-        info_window_html: render_to_string(partial: "info_window", locals: {restaurant: restaurant}),
-        marker_html: render_to_string(partial: "marker", locals: {restaurant: restaurant})
-      }
-
+    @city = params[:city]
     if params[:city].present? && params[:food].present?
       @restaurants = Restaurant.includes(:plates).where(plates: { id: Plate.select{|plate| plate.name.downcase.include?(params[:food].downcase)}.pluck(:id) }).select{|restaurant|restaurant.address.downcase.include?(params[:city].downcase)}# de este array de platos solo quiero los ID
     elsif params[:city].present? # "pizza"
-        @restaurants = Restaurant.select{|restaurant|restaurant.address.downcase.include?(params[:city].downcase)}# de este array de platos solo quiero los ID
+      @restaurants = Restaurant.select{|restaurant|restaurant.address.downcase.include?(params[:city].downcase)}# de este array de platos solo quiero los ID
     else
       @restaurants = Restaurant.all
-
     end
   end
-
   def show
     @restaurant = Restaurant.find(params[:id])
   end
-
   def new
     @restaurant = Restaurant.new
   end
-
   def create
     @restaurant = Restaurant.new(restaurant_params)
     @restaurant.user_id = current_user.id
-
     if @restaurant.save
       redirect_to my_restaurants_path(@restaurant), notice: 'Restaurant was successfully created.'
     else
       render :new, status: :unprocessable_entity
     end
   end
-
   def edit
     @restaurant = Restaurant.find(params[:id])
   end
-
   def update
     @restaurant = Restaurant.find(params[:id])
     if @restaurant.update(restaurant_params)
@@ -52,22 +36,17 @@ class RestaurantsController < ApplicationController
       render :edit, status: :unprocessable_entity
     end
   end
-
   def destroy
     @restaurant = Restaurant.find(params[:id])
     @restaurant.plates.destroy_all
     @restaurant.destroy
     redirect_to my_restaurants_path, notice: 'Restaurant was successfully destroyed.', status: :see_other
   end
-
   def my_restaurants
     @restaurants = Restaurant.where(user_id: current_user.id)
   end
-
   private
-
   def restaurant_params
     params.require(:restaurant).permit(:name, :address, :opening_date, :opening_time, :photo)
   end
-
 end
