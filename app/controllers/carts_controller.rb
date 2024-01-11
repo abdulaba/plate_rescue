bbf920b0789443a84f793dbad889a3a36f9d88e7
(Sofia Alfaro) sofiavapalfaro98@gmail.com <sofiavapalfaro98@gmail.com>
2024-01-11 13:01:37 -0500

    cart y selected plates funcionando - falta el total de tu cart y cantidades

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,7 @@
 class CartsController < ApplicationController
+  def show
+    @cart = Cart.find(current_user.carts.last.id)
+    @restaurant = @cart.plates.last.restaurant_id
+    @selected_items = @cart.selectedplates.includes(:plate)
+  end
 end
