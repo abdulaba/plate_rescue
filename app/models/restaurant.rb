dcbe72c61fcd764791c5fa58fe43539b1c0a4b7b
Francisco Macias <panchojmacias@gmail.com>
2024-01-15 11:20:58 -0300

    added review controller and model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,7 @@
 class Restaurant < ApplicationRecord
  # after_validation :geocode, if: :will_save_change_to_address?
- belongs_to :user; has_many :plates, dependent: :destroy; has_one_attached :photo
+  belongs_to :user; has_many :plates, dependent: :destroy; has_one_attached :photo
+  has_many :reviews
   validates :name, presence: true, length: { maximum: 100}
   validates :address, presence: true
   validates :user, presence: true
